2231c4b96c08930864f3f8cd78c3ed3a88f20e5f
Татьянин Никита Станиславович <tatianin.n.s@mac402-07.local>
2022-11-24 09:48:06 +0300

    82/82

---
+++ MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  Meditation
+//
+//  Created by Татьянин Никита Станиславович on 24/11/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
